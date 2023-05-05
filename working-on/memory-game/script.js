515a05d86080e6ba5a9511deffcd9e3efb2d4e81
Jaydon Yancey <jaydonyancey7@gmail.com>
2023-05-04 20:43:38 -0700

    start of memory game

---
+++ working-on/memory-game/script.js
@@ -0,0 +1,57 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'img/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'img/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'img/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'img/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'img/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'img/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'img/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'img/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'img/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'img/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'img/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'img/pizza.png'
+    }
+];
+
+// randomize the order of the array
+cardArray.sort(() => 0.5 - Math.random());
+
+const grid = document.querySelector('#grid');
+
+console.log(grid);
